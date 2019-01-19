ecc91f7c346f2f7a9c3f7acf68c85a8ef8c07bff
chenshenhai <cshenhai@gmail.com>
2019-01-19 15:21:15 +0800

    feat: update test

--- test/server_test.ts
+++ test/server_test.ts
@@ -4,11 +4,11 @@ import { run } from "deno";
 import { test, assert } from "https://raw.githubusercontent.com/denoland/deno_std/v0.2.6/testing/mod.ts";
 
 const httpServer = run({
-  args: ["deno", "--allow-net", "./test/server_example.ts", ".", "--cors"]
+  args: ["deno", "--allow-net", "./test/unit/server.ts", ".", "--cors"]
 });
 
 test(async function testServer() {
-  await new Promise(res => setTimeout(res, 5000));
+  await new Promise(res => setTimeout(res, 3000));
   const res = await fetch("http://127.0.0.1:4321");
   const text = await res.text();
   assert(text === 'hello');
