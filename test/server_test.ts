8074853a2af567e4c564111eb613127b490de4f9
chenshenhai <cshenhai@gmail.com>
2019-01-20 17:06:03 +0800

    feat: update test

--- test/server_test.ts
+++ test/server_test.ts
@@ -10,26 +10,27 @@ const httpServer = run({
 
 const testSite = "http://127.0.0.1:4321";
 
-test(async function testServer() {
+test(async function testServer_HelloWorld() {
   await new Promise(res => setTimeout(res, 1000));
   const res = await fetch(testSite);
   const text = await res.text();
   assert(text === "hello");
-  httpServer.close();
+  // httpServer.close();
 })
 
-// test(async function testServerContextData() {
-//   await new Promise(res => setTimeout(res, 1500));
-//   const queryMap = {
-//     key1: "123",
-//     key2: "abc",
-//   }
-//   const res = await fetch(`${testSite}/testCtxData.html?key1=123&key2=abc`);
-//   const json = await res.json();
-//   console.log('json ===', json);
-//   assert("hello" === "hello");
-//   httpServer.close();
-// })
+test(async function testServer_ContextData() {
+  await new Promise(res => setTimeout(res, 1500));
+  const queryMap = {
+    key1: "123",
+    key2: "abc",
+  }
+  const res = await fetch(`${testSite}/testCtxData.html?key1=123&key2=abc`);
+  const json = await res.json();
+  const resultStr = JSON.stringify(json)
+  const acceptStr = JSON.stringify(queryMap);
+  assert(resultStr === acceptStr);
+  httpServer.close();
+})
 
 
 
