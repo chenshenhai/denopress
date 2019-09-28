7ceab2f635d66be431405230e5deeea0a610abea
chenshenhai <cshenhai@gmail.com>
2019-09-28 17:42:25 +0800

    feat: add script_template

---
+++ core/template/script_template.ts
@@ -0,0 +1,107 @@
+// Copyright 2018-2019 chenshenhai. All rights reserved. MIT license.
+
+// This code has been ported almost directly from  https://github.com/chenshenhai/deno_note/blob/master/demo/template/mod.ts
+// Copyright 2017 The Authors. All rights reserved. MIT license.
+// https://github.com/chenshenhai/deno_note/blob/master/LICENSE
+
+
+const funcParamKey = "__DATA__";
+
+function compileToFunctionContentStr (tpl) {
+  const tplCode = tpl;
+  const regTpl = /#if[\s]{0,}\(([^\)]+)?\)|#elseif[\s]{0,}\(([^\)]+)?\)|#else|\(([^\)]+)?\)|#foreach[\s]{0,}\(([^\)]+)?\)\.indexAs\(([^\)]+)?\)|#foreach[\s]{0,}\(([^\)]+)?\)\.keyAs\(([^\)]+)?\)|{{([^}}]+)?}}|#\/if|#\/foreach/ig;
+  const regDirectEnd = /#\/if|#\/foreach/i;
+  const regDirectIf = /#if[\s]{0,}\(([^\)]+)?\)/i;
+  const regDirectElse = /#else[\s]{0,}/i;
+  const regDirectElseif = /#elseif[\s]{0,}\(([^\)]+)?\)/i;
+  const regDirectForArray = /#foreach[\s]{0,}\(([^\)]+)?\)\.indexAs\(([^\)]+)?\)/i;
+  const regDirectForJSON = /#foreach[\s]{0,}\(([^\)]+)?\)\.keyAs\(([^\)]+)?\)/i;
+  const regData = /{{([^}}]+)?}}/i;
+  const directiveStock = [];
+  let funcCodeStr = "";
+  let match = true;
+  let codeIndex = 0;
+  funcCodeStr += "\r\n let _row=[];\r\n";
+
+  const addFuncCode = function (params) {
+    const { currentExec, restCode } = params;
+
+    if (regData.test(currentExec) === true) {
+      // set data
+      funcCodeStr += `\r\n _row.push(${regData.exec(currentExec)[1]});`;
+    } else if (regDirectIf.test(currentExec) === true) {
+      funcCodeStr += `\r\n if ( ${regDirectIf.exec(currentExec)[1]} ) {`;
+      directiveStock.push("if");
+    } else if (regDirectElseif.test(currentExec) === true) {
+      funcCodeStr += `\r\n } else if ( ${regDirectElseif.exec(currentExec)[1]} ) {`;
+    } else if (regDirectElse.test(currentExec) === true) {
+      funcCodeStr += `\r\n } else {`;
+    } else if (regDirectForArray.test(currentExec) === true) {
+      const forArrayName = regDirectForArray.exec(currentExec)[1];
+      const forArrayIndexName = regDirectForArray.exec(currentExec)[2] || "idx";
+      funcCodeStr += `
+      \r\n for ( let ${forArrayIndexName}=0; ${forArrayIndexName}<${forArrayName}.length; ${forArrayIndexName}++ ) {
+      `;
+      directiveStock.push("for-array");
+    } else if (regDirectForJSON.test(currentExec) === true) {
+      const forJSONName = regDirectForJSON.exec(currentExec)[1];
+      const forJSONKey = regDirectForJSON.exec(currentExec)[2] || "key";
+      funcCodeStr += `
+      \r\n for ( const ${forJSONKey} in ${forJSONName} ) {
+      `;
+      directiveStock.push("for-json");
+    } else if (regDirectEnd.test(currentExec) === true) {
+      funcCodeStr += `\r\n }`;
+      directiveStock.pop();
+    } else {
+      funcCodeStr += `\r\n _row.push(\`${restCode}\`); `;
+    }
+  };
+
+  let excecResult;
+  while (match) {
+    // console.log(`tplCode = ${tplCode} \r\r\n`)
+    excecResult = regTpl.exec(tplCode);
+    if (match && excecResult) {
+      const restCode = tplCode.slice(codeIndex, excecResult.index);
+      const currentExec = excecResult[0];
+      const currentMatch = excecResult[1];
+      addFuncCode({ restCode });
+      addFuncCode({ currentExec, currentMatch, restCode });
+      codeIndex = excecResult.index + excecResult[0].length;
+    } else {
+      match = false;
+    }
+  }
+  addFuncCode({ restCode: tplCode.substr(codeIndex, tplCode.length) });
+
+  funcCodeStr += `\r\n return _row.join("");`;
+  funcCodeStr = funcCodeStr.replace(/[\r\t\r\n]/g, "");
+  return funcCodeStr;
+}
+
+function compileToFunctionInlineDataStr (data: object): string  {
+  const resultList = [];
+  if (Object.prototype.toString.call(data).toLocaleLowerCase() === '[object object]') {
+    const keys: string [] = Object.keys(data);
+    keys.forEach(function(key) {
+      if (typeof key === "string" && key.length > 0) {
+        resultList.push(`const ${key} = ${funcParamKey}.${key}; \r\n`);
+      }
+    })
+  }
+  const resultStr = resultList.join('\r\n');
+  return resultStr;
+}
+
+const template = {
+  compile (tpl, data) {
+    const funcDataStr = compileToFunctionInlineDataStr(data);
+    const funcContentStr = compileToFunctionContentStr(tpl);
+    const funcStr = [funcDataStr, funcContentStr].join('\r\n');
+    const func = new Function(funcParamKey, funcStr.replace(/[\r\t\r\n]/g, ""));
+    const html = func(data);
+    return html;
+  }
+};
+export const compileTemplate = template.compile;
---
+++ core/template/script_template_test.ts
@@ -0,0 +1,96 @@
+// Copyright 2018-2019 chenshenhai. All rights reserved. MIT license.
+
+// This code has been ported almost directly from  https://github.com/chenshenhai/deno_note/blob/master/demo/template/test.ts
+// Copyright 2017 The Authors. All rights reserved. MIT license.
+// https://github.com/chenshenhai/deno_note/blob/master/LICENSE
+
+import { testing, asserts } from "./../../deps.ts";
+import { compileTemplate } from "./script_template.ts";
+
+const { test, runTests } = testing;
+const { equal } = asserts;
+
+
+test(function testCompileTemplate() {
+  const tpl = `
+  <div>
+    #if( datalist && datalist.length > 0 )
+      <ul>
+      #foreach(datalist).indexAs(i)
+        <li>{{i}}:{{datalist[i]}}</li>
+      #/foreach
+      </ul>
+    #elseif( showOthers === true )
+      <p> others </p>
+    #else
+      <p> default </p>
+    #/if
+
+    #if( dataChildList && dataChildList.length > 0 )
+      <div>
+      #foreach(dataChildList).indexAs(i)
+        <ul>
+        #if(dataChildList[i].children)
+          #foreach(dataChildList[i].children).indexAs(j)
+            <li>{{i}}-{{j}} : {{dataChildList[i].children[j].name}}</li>
+          #/foreach
+        #/if
+        </ul>
+      #/foreach
+      </div>
+    #/if
+
+    #if( datajson)
+      <ul>
+      #foreach(datajson).keyAs(k)
+        <li>{{k}}:{{datajson[k]}}</li>
+      #/foreach
+      </ul>
+    #/if
+  </div>`;
+  const data = {
+    title: "helloworld",
+    text: "hellopage",
+    isShowDataList: true,
+    datalist: [
+      "item1", "item2", "item3"
+    ],
+    dataChildList: [
+      {
+        name: "item-1",
+        children: [
+          {
+            name: "child-1-1",
+          },
+          {
+            name: "child-1-2",
+          }
+        ]
+      },
+      {
+        name: "item-2",
+        children: [
+          {
+            name: "child-2-1",
+          },
+          {
+            name: "child-2-2",
+          },
+          {
+            name: "child-3-2",
+          }
+        ]
+      }
+    ],
+    datajson: {
+      "key1": "val1",
+      "key2": "val2"
+    }
+  };
+  const html = compileTemplate(tpl, data);
+  const expectedResult = `  <div>          <ul>              <li>0:item1</li>              <li>1:item2</li>              <li>2:item3</li>            </ul>              <div>              <ul>                              <li>0-0 : child-1-1</li>                      <li>0-1 : child-1-2</li>                          </ul>              <ul>                              <li>1-0 : child-2-1</li>                      <li>1-1 : child-2-2</li>                      <li>1-2 : child-3-2</li>                          </ul>            </div>              <ul>              <li>key1:val1</li>              <li>key2:val2</li>            </ul>      </div>`;
+  equal(expectedResult, html);
+  equal(1, 1);
+});
+
+runTests();
