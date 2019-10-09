a09478269d440f6cdd667f31e7c05c1469eefa3e
chenshenhai <cshenhai@gmail.com>
2019-10-09 23:50:16 +0800

    deps: upgrade deno_std@0.20.0

--- deps.ts
+++ deps.ts
@@ -1,9 +1,9 @@
-import * as server from "https://deno.land/std@v0.19.0/http/server.ts";
-import * as cookie from "https://deno.land/std@v0.19.0/http/cookie.ts";
-import * as bufio from "https://deno.land/std@v0.19.0/io/bufio.ts";
-import * as fs from "https://deno.land/std@v0.19.0/fs/mod.ts";
-import * as testing from "https://deno.land/std@v0.19.0/testing/mod.ts";
-import * as asserts from "https://deno.land/std@v0.19.0/testing/asserts.ts";
+import * as server from "https://deno.land/std@v0.20.0/http/server.ts";
+import * as cookie from "https://deno.land/std@v0.20.0/http/cookie.ts";
+import * as bufio from "https://deno.land/std@v0.20.0/io/bufio.ts";
+import * as fs from "https://deno.land/std@v0.20.0/fs/mod.ts";
+import * as testing from "https://deno.land/std@v0.20.0/testing/mod.ts";
+import * as asserts from "https://deno.land/std@v0.20.0/testing/asserts.ts";
 
 // import * as mysql from "https://deno.land/x/mysql@1.2.3/mod.ts";
 
