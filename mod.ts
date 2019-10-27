9645daac2ba4fdc92cb06447ce3d85871b644ba7
chenshenhai <cshenhai@gmail.com>
2019-10-27 21:31:55 +0800

    feat: update denopress cli

--- mod.ts
+++ mod.ts
@@ -5,11 +5,26 @@ const args = Deno.args;
 const baseDir: string = Deno.cwd();
 const order = args[1]; // TODO
 
+
+function showHelp(): void {
+  console.log(`
+denopress
+  A simple theme web based on Deno
+USAGE:
+  denopress  [SUBCOMMAND]
+SUBCOMMANDS:
+  init           initialize a denopress.json
+  start          load theme basic resources and start web
+`);
+}
+
 async function main() {
   if (order === 'init') {
     init(baseDir);
   } else if (order === 'start') {
     start(baseDir);
+  } else {
+    showHelp();
   }
 }
 
