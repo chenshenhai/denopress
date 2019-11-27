bf38f418d06ddd49cd66a5dcf03d9d9d4556e2f9
chenshenhai <cshenhai@gmail.com>
2019-11-27 23:11:11 +0800

    feat: add md5 lib

---
+++ core/util/md5.d.ts
@@ -0,0 +1,1 @@
+export function md5(string: string, key?: string, raw?: string): string;
--- server/service/service/user.ts
+++ server/service/service/user.ts
@@ -1,8 +1,10 @@
 import { BaseModel } from "./../../model/base_model.ts";
+import { md5 } from "./../../../core/util/md5.js";
 
 export function createUserService(models: {[key: string]: BaseModel}) {
   const serivce = {
     async create(data: {[key: string]: string|number}) {
+      data.password = md5(data.password as string);
       const result = {
         success: true,
         data: null,
